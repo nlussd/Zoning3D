d896b469b70b75ea0ce6e69164217006bd93e827
nlussd <lusianasuwandi@gmail.com>
2017-09-18 20:09:40 +0700

    udah jadi search box

--- js/viz.js
+++ js/viz.js
@@ -1,39 +1,28 @@
   $(document).ready(function(){
-          $('.clickBox').hover(function(){
+          $('.clickBox, .clickBoxMain').hover(function(){
               $(this).css("background-color", "#a6c4fc");
               }, function(){
               $(this).css("background-color", "#3285ff");
           });
           $('.clickBox').click(function(){
-              $('.clickBox').hide();
-              $('.welcome').hide();
-              $('#button').fadeIn();
-              $('#chartdiv').fadeIn();
-              $('#chartdiv2').fadeIn();
-              $('.clickBoxMain').fadeIn();
-              $('.desc').fadeIn();
-              $('.summary').fadeIn();
+              $('.clickBox, .welcome').hide();
+              $('#button, #chartdiv, #chartdiv2, .clickBoxMain, .desc, .summary').fadeIn();
+              $('.descTxt').text("We can see charts and statistics of zoning areas here");
           });
-          $('.clickBoxMain.clickHome').hover(function(){
-              $(this).css("background-color", "#a6c4fc");
-              }, function(){
-              $(this).css("background-color", "#3285ff");
-          });
-          $('.clickBoxMain.clickHome').click(function(){
-              $('#chartdiv').hide();
-              $('#chartdiv2').hide();
-              $('.clickBoxMain').hide();
-              $('.desc').hide();
-              $('.summary').hide();
-              $('#summaryTxt').hide();
-              $('.welcome').show();
-              $('.clickBox').show();
+          $('.clickHome').click(function(){
+              $('#chartdiv, #chartdiv2, .clickBoxMain, .desc, .summary, #summaryTxt, #search, #execute').hide();
+              $('.welcome, .clickBox').show();
           });
           $('.summary').click(function(){
-              $('#chartdiv').toggle();
-              $('#chartdiv2').toggle();
-              $('#summaryTxt').toggle();
+              $('#chartdiv, #chartdiv2, #summaryTxt').toggle();
           });
+          $('.clickSearch').click(function(){
+              $('.clickBox, .welcome, #button, #chartdiv, #chartdiv2, .summary, #summaryTxt').hide();
+              $('.desc').show();
+              $('.descTxt').text("We can search for individual building information. For example to see if the existing building fits into the zoning regulation. Type building name in the box below.");
+              $('#search, #execute').fadeIn();
+          });
+
 
 
          
